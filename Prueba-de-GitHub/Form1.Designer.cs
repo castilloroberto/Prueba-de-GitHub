b33d75b98fa1869d53d7dd965af08b298ce8dc76
ivisvalle <ivisvalle2018@gmail.com>
2021-05-22 16:44:12 -0600

    cambie estilos del titulo

--- Prueba-de-GitHub/Form1.Designer.cs
+++ Prueba-de-GitHub/Form1.Designer.cs
@@ -36,9 +36,11 @@ namespace Prueba_de_GitHub
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(289, 74);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 29F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(169, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.Size = new System.Drawing.Size(409, 66);
             this.label1.TabIndex = 0;
             this.label1.Text = "Prueba de Github";
             // 
